df653e1edb0b57a4d8a64e17cfb28701ebc8c1a6
yamil dev <83871101+yamildev@users.noreply.github.com>
2023-05-08 17:24:52 -0300

    Allow to make only one operation     -We need to improve the program to behave a normal functionaity         -need to do how much operations the user want

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # calculator
-This is a browser calculator 
+Ahora estaria necesitando que el 'displayValue' se muestre
+graficamente en 'currentOperation' pero en current 
+no deberian mostrarse ya los valores, pasados a number
+etc, etc... ? Bueno, mejor no. Vamos paso a paso
--- scripts.js
+++ scripts.js
@@ -1,48 +1,79 @@
-/*Hi, i'm in calculator project and i'm in the part
-5. Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
- */
+let currentInnerText = ''
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
+let innerText = "";
 
+const buttons = document.querySelectorAll('.buttons-grid')
+const previousOperationScreen = document.getElementById("previous-operation")
+const currentOperationScreen = document.getElementById("current-operation")
+// expresión regular para coincidir con números positivos o negativos, con decimales o no entre 0 y 9.
+const numberRegex = /^-?\d*\.?\d+$/;
 
-const buttons = document.querySelectorAll(".buttons-grid")
+function clear(something) {
+  if (something === 'CLEAR')
+    currentOperationScreen.innerText = ""
+  previousOperationScreen.innerText = ""
 
-buttons.forEach((button) =>{
+}
+
+/*
+while (!firstOperand.includes(objToArr(operators))) {
+      firstOperand += displayValue
+    }
+*/
+buttons.forEach((button) => {
   button.addEventListener('click', e => {
-    return console.log(e.target.innerText)
+    //mostrar contenido en currentOperationScreen
+    currentInnerText = e.target.innerText
+    console.log('currentInnerText: ',currentInnerText)
+    currentOperationScreen.textContent += currentInnerText
+    console.log('currentOperationScreen: ', currentOperationScreen.textContent)
+
+    //variables storing
+    if (operators.includes(currentInnerText)) {
+      operator = currentInnerText //stores the operator, si eligo un operador, guardalo 
+      console.log('operator: ', operator)     
+
+    } else if(operator == '') {
+
+      firstOperand += parseFloat(currentInnerText)
+      console.log('firstOperand: ', firstOperand) 
+      console.log('firstOperand type: ', typeof(firstOperand))     
+      
+    } else {
+      secondOperand += parseFloat(currentInnerText)
+      parseFloat(secondOperand)
+      console.log('secondOperand: ', secondOperand)
+      console.log('secondOperand type: ', typeof(secondOperand))     
+
+    }
+    //run operate()
+    if (currentInnerText == '=') {
+      currentOperationScreen.textContent = operate(operator, parseFloat(firstOperand), parseFloat(secondOperand))
+      console.log(currentOperationScreen)
+    }
+    console.log('*****************************************************************************')
+    clear(currentInnerText);
+
   })
-})
+});
 
-
-
-/*
-3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable 
-for the first number, the operator, and the second number. You’ll use these variables to update 
-your display later.
-*/
-let firstOperand = null;
-let secondOperand = null;
-let operand = null;
-
-/*
-Create a new function operate that takes an operator and 2 numbers and then calls one of the above
- functions on the numbers.
-*/
 function operate(operator, a, b) {
-    //probablemente deba asegurarme  de que
-    //ingresen los parametros en tipo Number
-    // a = Number(a) ??????????? averiguar
-    switch (operator) {
-        case '+': 
-            return add(a,b);
-        case '−': 
-            return substract(a,b);
-        case '×': 
-            return multiply(a,b);
-        case '÷':
-            if (b === 0) return null
-            else return divide(a, b)
 
-        default: return null 
-}              
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '−':
+      return substract(a, b);
+    case '×':
+      return multiply(a, b);
+    case '÷':
+      if (b === 0) return null
+      else return divide(a, b)
+
+    default: return null
+  }
 }
 
 //          BASIC OPERATIONS
@@ -62,28 +93,19 @@ function divide(a, b) {
   return a / b
 }
 
+const operators = ['÷','−','×','+']
 
-
-
-/*
-Create the functions that populate the
-display when you click the number buttons.
-You should be storing the ‘display value’ 
-in a variable somewhere for use in the next step.
-
-(?)Divide and conquer. Que puedo ir solucionando?
--seleccionar los botones del teclado
-  + necesito que se dispare
--seleccionar las dos pantallas
-
-(?) accederas al teclado a traves del attr textContent
+/*               codigo de validacion segun openAI
+    if (!isNaN(parseFloat(currentInnerText))) { //if currentInnerText is a number, do this:
+      if (inner === '') { //If an operator doesn't still selected
+        // if(currentInnerText === )
+        firstOperand += currentInnerText
+        console.log('firstOperand: ', firstOperand)
+        console.log('typeof', typeof (firstOperand))
+        //si innerText es igual a alguno de los valores del objeto,
+        //agregar el resultado a operator
+      } else {
+        secondOperand += currentInnerText
+      }
+    }
 */
-//(?) debes seleccionar la nodeList con un forEach() --listo
-// y un addEventListener que responda al evento click (2da parte: evento por keyInput --listo
-
-
-
-
-
-
-  
